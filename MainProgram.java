617a682c7e1070a2aa7096e871e9dbb54ffa382a
grlyntng <53943829+grlyntng@users.noreply.github.com>
2023-06-12 02:04:05 +0800

    Tested getting input and displaying it

--- MainProgram.java
+++ MainProgram.java
@@ -9,7 +9,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.border.*;
 
-public class MainProgram{
+public class MainProgram {
 
     public static void main(String[] args){
         JFrame program_frame = new JFrame(); //creating one new window where everything takes place
@@ -40,14 +40,24 @@ public class MainProgram{
         // Component 1: Input (must accept 1-10 variables/states, 1-3 alphabet and Epsilon)
         String[] states = {"A","A,B","A,B,C","A,B,C,D","A,B,C,D,E","A,B,C,D,E,F","A,B,C,D,E,F,G","A,B,C,D,E,F,G,H","A,B,C,D,E,F,G,H,I","A,B,C,D,E,F,G,H,I,J"};//Q
         String[] alphabets = {"0,1","0,1,2","0,1,2,3"}; //∑
+        String[] initials= {"A","B","C","D","E","F","G","H","I","J"};
         JLabel fa_label1 = new JLabel("Q=");
         fa_label1.setBounds(100,14, 80,30); 
         JLabel fa_label2 =new JLabel("∑=");    
         fa_label2.setBounds(100,34, 80,30);
-        JComboBox states_input = new JComboBox(states);
+        JLabel fa_label3 = new JLabel("Initial state=");
+        fa_label3.setBounds(100,54, 97,30);
+        JLabel fa_label4 = new JLabel("Final state=");
+        fa_label4.setBounds(100,74, 80,30);
+
+        JComboBox<String> states_input = new JComboBox<>(states);
         states_input.setBounds(120,20, 130,20);
-        JComboBox alphabets_input = new JComboBox(alphabets);
+        JComboBox<String> alphabets_input = new JComboBox<>(alphabets);
         alphabets_input.setBounds(120,40, 100,20);
+        JComboBox<String> initial_input = new JComboBox<>(initials);
+        initial_input.setBounds(180, 60, 60,20);
+        JComboBox<String> final_input = new JComboBox<>(initials);
+        final_input.setBounds(180, 80, 60,20);
 
         
         // Component 2: Regular Grammar Conversion Output
@@ -59,6 +69,9 @@ public class MainProgram{
         b2.setBounds(5,40, 80,25);
         JButton b3 = new JButton("RG");  // on click, submit input to generate rg output
         b3.setBounds(5,70, 80,25);
+       
+        JLabel output1 = new JLabel();
+        fa_rg_page.add(output1);
         fa_rg_page.add(b1);
         fa_rg_page.add(b2);
         fa_rg_page.add(b3);
@@ -66,6 +79,31 @@ public class MainProgram{
         fa_rg_page.add(states_input);
         fa_rg_page.add(fa_label2);
         fa_rg_page.add(alphabets_input);
+        fa_rg_page.add(fa_label3);
+        fa_rg_page.add(initial_input);
+        fa_rg_page.add(fa_label4);
+        fa_rg_page.add(final_input);
+
+        //upon clicking b3
+        b3.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                String states_choice = states_input.getSelectedItem().toString();
+                String alphabets_choice = alphabets_input.getSelectedItem().toString();
+                String initial_choice = initial_input.getSelectedItem().toString();
+                String final_choice = final_input.getSelectedItem().toString();
+        
+                output1.setText("The output is: " + states_choice +", "+alphabets_choice+", "+initial_choice+", "+final_choice);
+                output1.setBounds(300, 10, 200, 100);
+                
+            }
+        });
+
+        
+        
+
+
+
+
         //panel for CYK page
         JPanel cyk_page = new JPanel();
 
@@ -86,5 +124,9 @@ public class MainProgram{
         
         
     }
+
+
+ 
+
 }
 

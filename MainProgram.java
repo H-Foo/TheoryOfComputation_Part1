49edaffe7de3147cffd9ce0306db15133650d8c0
grlyntng <53943829+grlyntng@users.noreply.github.com>
2023-06-20 21:08:13 +0800

    integrated other tabs

--- MainProgram.java
+++ MainProgram.java
@@ -3,6 +3,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
@@ -12,7 +15,7 @@ import javax.swing.border.*;
 
 public class MainProgram {
 
-    
+    private static Set<String>[][] chart;
 
     public static void main(String[] args){
         JFrame program_frame = new JFrame(); //creating one new window where everything takes place
@@ -23,19 +26,56 @@ public class MainProgram {
 
         //panel for home page
         JPanel home_page = new JPanel();        
-        BoxLayout boxlayout1 = new BoxLayout(home_page, BoxLayout.Y_AXIS);// Creating Object of "boxlayout" in Y_Axis from top to down
-        home_page.setLayout(boxlayout1);
-        home_page.setBorder(new EmptyBorder(new Insets(5, 120, 100, 100)));
-        JLabel home_label1 = new JLabel("<html>TIC2151 Theory of Computation <br/> Tut. no. <br/> Group no.<br/> Group Members, Name, photo and ID </html>",SwingConstants.CENTER);
-        JLabel home_label2 = new JLabel("content");
-        JLabel home_label3 = new JLabel("Other info such as members participation");
-        JLabel home_label4 = new JLabel("<html>Member 1 = 25% <br/> Member 2 = 25% <br/> Member 3 = 25% <br/> Member 4 = 25%</html>",SwingConstants.CENTER);
+        home_page.setLayout(null);
+        
+        //scaling image
+        ImageIcon imageIcon = new ImageIcon("image.jpg");
+        Image originalImage = imageIcon.getImage();
+        int icon_width = 75;
+        int icon_height = 75;
+        Image finalImage = originalImage.getScaledInstance(icon_width, icon_height, Image.SCALE_SMOOTH);
+        
+        //set icons
+        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+        Border border2 = BorderFactory.createLineBorder(Color.BLACK, 2);
+        JLabel home_label2a = new JLabel();
+        home_label2a.setIcon(new ImageIcon(finalImage));
+        home_label2a.setBorder(border2);
+        JLabel home_label2b = new JLabel();
+        home_label2b.setIcon(new ImageIcon(finalImage));
+        home_label2b.setBorder(border);
+        JLabel home_label2c = new JLabel();
+        home_label2c.setIcon(new ImageIcon(finalImage));
+        home_label2c.setBorder(border);
+        JLabel home_label2d = new JLabel();
+        home_label2d.setIcon(new ImageIcon(finalImage));
+        home_label2d.setBorder(border);
+        
+        JLabel home_label1 = new JLabel("<html>TIC2151 Theory of Computation <br/> Tut. no. TT1L <br/> Group no.2 </html>",SwingConstants.CENTER);
+        JLabel home_label2 = new JLabel("Geralyn Ting        Humaira' Foo        Yap Han Yang       Yan Jac Kie ");
+        JLabel home_label3 = new JLabel(" 1181102223            1201301955            1181102575         1191201793 ");
+        JLabel home_label4 = new JLabel("<html>Participation: <br/> Geralyn Ting = 25% - Q1 <br/> Humaira' Foo = 25% - Q1 <br/> Yap Han Yang = 30% - Q2<br/> Yan Jac Kie = 20% - Q2</html>",SwingConstants.CENTER);
+        JLabel home_label5 = new JLabel("Leader");
+
+        home_label1.setBounds(90,15, 200,100);
+        home_label2.setBounds(100,100, 400,30);
+        home_label3.setBounds(100,120, 400,30);
+        home_label2a.setBounds(100,150, 75,75);
+        home_label2b.setBounds(200,150, 75,75);
+        home_label2c.setBounds(300,150, 75,75);
+        home_label2d.setBounds(400,150, 75,75);
+        home_label5.setBounds(100,220, 100,30);
+        home_label4.setBounds(70,200, 200,200);
+        
         home_page.add(home_label1);
         home_page.add(home_label2);
+        home_page.add(home_label2a);
+        home_page.add(home_label2b);
+        home_page.add(home_label2c);
+        home_page.add(home_label2d);
         home_page.add(home_label3);
         home_page.add(home_label4);
-        
-        
+        home_page.add(home_label5);
         
         //panel for FA to RG page
         JPanel fa_rg_page = new JPanel();
@@ -292,16 +332,80 @@ public class MainProgram {
         });
         
         
-
-
-
-
         //panel for CYK page
         JPanel cyk_page = new JPanel();
+        cyk_page.setLayout(null);
+        JButton b7 = new JButton("Import");
+        b7.setBounds(5,10, 80,25);
+        JButton b5 = new JButton("Clear");  // on click, clear program
+        b5.setBounds(5,40, 80,25);
+        JButton b6 = new JButton("Check");  // on click, submit input to generate rg output
+        b6.setBounds(5,70, 80,25);
+        cyk_page.add(b7);
+        cyk_page.add(b5);
+        cyk_page.add(b6);
+        
+        JTextArea CNF_input = new JTextArea();
+        CNF_input.setBounds(120,20, 200,200);
+       // CNF_input.setSize(200,200);
+        cyk_page.add(CNF_input);
+        
+        JTextField inputStringField = new JTextField();
+        inputStringField.setBounds(120, 230, 200, 20);
+        cyk_page.add(inputStringField);
 
+        JTextArea cyk_display = new JTextArea();
+        JScrollPane scrollbarj = new JScrollPane(cyk_display);
+        scrollbarj.setBounds(120, 250, 400, 180);
+        cyk_page.add(scrollbarj);
+        
+        // Example grammar in CNF
+        java.util.List<Rule> grammar = new ArrayList<>();
+        grammar.add(new Rule("S", "AB"));
+        grammar.add(new Rule("S", "BC"));
+        grammar.add(new Rule("A", "BA"));
+        grammar.add(new Rule("A", "a"));
+        grammar.add(new Rule("B", "CC"));
+        grammar.add(new Rule("B", "b"));
+        grammar.add(new Rule("C", "AB"));
+        grammar.add(new Rule("C", "a"));
+
+        b7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        b5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CNF_input.setText("");
+                cyk_display.setText("");
+                inputStringField.setText("");
+            }
+        });
+        
+        b6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputString = inputStringField.getText();
+                String cnfGrammar = CNF_input.getText();
+
+                // Parse CNF grammar
+                java.util.List<Rule> grammar = parseCNFGrammar(cnfGrammar);
+
+                // Generate CYK chart
+                generateCYKChart(grammar, inputString);
+
+                // Print CYK chart and result
+                printCYKChart(inputString, cyk_display);
+            }
+        });
 
         //panel to help page
         JPanel help_page = new JPanel();
+        JLabel help = new JLabel("<html>1. FA->RG: Avoid clicking 'New' more than once as it will append the table into itself. In this case, select 'Clear'. <br/> 2. To start a new instance, remember to select 'Clear'. <br/> 3. For the user manual, please refer to the application report  <html>");
+        help_page.add(help);
+
 
         tp.addTab("Home",home_page);
         tp.addTab("FA -> RG",fa_rg_page);
@@ -314,10 +418,120 @@ public class MainProgram {
         program_frame.setVisible(true);
         program_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
-        
     }
 
+        private static void generateCYKChart(java.util.List<Rule> grammar, String inputString) {
+        int n = inputString.length();
+        chart = new HashSet[n][n];
 
+        // Initialize chart
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chart[i][j] = new HashSet<>();
+            }
+        }
+
+        // Fill in the diagonal of the chart with terminals that generate the string
+        for (int i = 0; i < n; i++) {
+            for (Rule rule : grammar) {
+                if (rule.getRhs().length() == 1 && rule.getRhs().equals(String.valueOf(inputString.charAt(i)))) {
+                    chart[i][i].add(rule.getLhs());
+                }
+            }
+        }
+
+        // Fill in the rest of the chart
+        for (int length = 2; length <= n; length++) {
+            for (int start = 0; start <= n - length; start++) {
+                int end = start + length - 1;
+                for (int split = start; split < end; split++) {
+                    for (Rule rule : grammar) {
+                        if (rule.getRhs().length() == 2) {
+                            String A = rule.getLhs();
+                            String B = rule.getRhs().substring(0, 1);
+                            String C = rule.getRhs().substring(1);
+
+                            if (chart[start][split].contains(B) && chart[split + 1][end].contains(C)) {
+                                chart[start][end].add(A);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+private static java.util.List<Rule> parseCNFGrammar(String cnfGrammar) {
+    java.util.List<Rule> grammar = new ArrayList<>();
+
+    // Split the input by newlines to get individual rules
+    String[] ruleLines = cnfGrammar.split("\\r?\\n");
+
+    for (String line : ruleLines) {
+        // Split each rule into LHS and RHS
+        String[] ruleParts = line.split("->");
+        String lhs = ruleParts[0].trim();
+        String[] rhsParts = ruleParts[1].split("\\|");
+        
+        for (String rhs : rhsParts) {
+            grammar.add(new Rule(lhs, rhs.trim()));
+        }
+    }
+
+    return grammar;
+}
+
+    public static void printCYKChart(String inputString, JTextArea cyk_display) {
+        int n = inputString.length();
+
+        StringBuilder chartOutput = new StringBuilder();
+
+        chartOutput.append("\t");
+        for (int j = 0; j < n; j++) {
+            chartOutput.append(inputString.charAt(j)).append("\t");
+        }
+        chartOutput.append("\n");
+
+        for (int i = 0; i < n; i++) {
+            chartOutput.append(i).append("\t");
+            for (int j = 0; j < n - i; j++) {
+                chartOutput.append("|");
+                for (String value : chart[j][j + i]) {
+                    chartOutput.append(value).append(" ");
+                }
+                chartOutput.append("\t");
+            }
+            chartOutput.append("|");
+            chartOutput.append("\n");
+        }
+
+        if (chart[0][n - 1].contains("S")) {
+            chartOutput.append("The string can be generated by the grammar.");
+        } else {
+            chartOutput.append("The string cannot be generated by the grammar.");
+        }
+
+        cyk_display.setText(chartOutput.toString());
+    }
+
+    static class Rule {
+        private String lhs;
+        private String rhs;
+
+        public Rule(String lhs, String rhs) {
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+
+        public String getLhs() {
+            return lhs;
+        }
+
+        public String getRhs() {
+            return rhs;
+
+        }
+    }
  
 
 }
